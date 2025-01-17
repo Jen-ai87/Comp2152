e0f3635da3fc28e74226749a985e85cf317a9212
Jen Henry <136381247+Jen-ai87@users.noreply.github.com>
2025-01-16 21:05:43 -0500

    Lab Week 1 - Coding Questions

---
+++ Week01/lab1.py
@@ -0,0 +1,23 @@
+# Sample Coding Questions 01 Week 01
+# First Name: Jen 
+# Last Name: Henry
+
+
+# Defining Variables
+element_array = [1, 4, 7, 9] 
+
+# Order of Operations - Brackedted version of e = a - b ** c // d + a % c
+a = 1, b = 2, c = 3, d = 4
+e = (((a - (b ** c)) // d) + (a % c))
+
+
+# Formatting
+temperature = 32.6
+print(f"The temperature today is: {temperature:.3f} degrees Celsius")
+
+# Common Functions
+userAge = int(input("Enter your age: ")) 
+
+filteredAge = userAge + 22
+
+print(f"Now showing the shop items filtered by age: {filteredAge}")
